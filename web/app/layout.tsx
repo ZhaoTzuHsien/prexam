ecf26a24b2c4eea56bda51e452c299133281628a
趙子賢 <danny900714@gmail.com>
2023-07-21 11:40:27 +0800

    Add Tailwind UI dependencies

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </head>
       <body>{children}</body>
     </html>
   )
